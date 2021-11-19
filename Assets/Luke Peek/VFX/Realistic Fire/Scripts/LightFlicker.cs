8bbddb6259aa06fa1dccc1c051dac581d8373352
muzdavines <michaeluzdavines@gmail.com>
2021-11-18 19:19:41 -0500

    Particles

---
+++ Assets/Luke Peek/VFX/Realistic Fire/Scripts/LightFlicker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+	
+	
+	public bool flicker = true;
+	
+	public float flickerIntensity = 0.5f;
+	
+	private float baseIntensity;
+	private Light lightComp;
+	
+	
+	void Awake()
+	{
+		lightComp = gameObject.GetComponent<Light>();
+		baseIntensity = lightComp.intensity;
+	}
+	
+	
+	void Update ()
+	{
+		if (flicker)
+		{
+			float noise = Mathf.PerlinNoise(Random.Range(0.0f, 1000.0f), Time.time);
+			lightComp.intensity = Mathf.Lerp( baseIntensity - flickerIntensity , baseIntensity, noise );
+		}
+	}
+}
